409807f185368f5dd6115b992728ca076ec1f844
Abner Teles <abner.teles@it2a.com>
2026-09-22 11:12:17 -0300

    infra: activate https listener 443 with issued acm ssl cert and http redirect

--- infra/terraform/acm.tf
+++ infra/terraform/acm.tf
@@ -18,16 +18,16 @@ resource "aws_acm_certificate" "api_cert" {
   }
 }
 
-# Listener HTTPS será ativado assim que a validação do DNS for concluída na IONOS
-# resource "aws_lb_listener" "https" {
-#   load_balancer_arn = aws_lb.api.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   certificate_arn   = aws_acm_certificate.api_cert.arn
-# 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.api.arn
-#   }
-# }
+# Listener HTTPS ativo na Porta 443 do ALB
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.api_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
